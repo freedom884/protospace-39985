a07b4efae2454731a2ff845429b7dbdf8d4ad117
freedom884 <bass.ran.003@gmail.com>
2024-01-15 13:54:07 +0900

    initial commi

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,9 @@ class CommentsController < ApplicationController
     end
   end
     
-
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
   
-
 end
