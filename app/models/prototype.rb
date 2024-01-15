2d9c24472d4244ceb134644b0d1dfbfdf3eec742
freedom884 <bass.ran.003@gmail.com>
2024-01-15 17:55:20 +0900

    initial commit2

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,12 @@ class Prototype < ApplicationRecord
   validates :image, presence: true
   has_many :comments , dependent: :destroy 
 
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+
+
+
   def create
     @prototype = Prototype.new(prototype_params)
     # 以下略
